e9428191f80945a453482cc76bc8171b0ba5fedf
Hiroaki Nakamura <hnakamur@gmail.com>
2012-05-23 15:06:05 +0900

    Add Redmine 2.0.0 support

--- redmine/recipes/default.rb
+++ redmine/recipes/default.rb
@@ -6,17 +6,53 @@
 #
 
 version = node[:redmine][:version]
+ruby_path = node[:ruby][:path]
 session_secret = node[:redmine][:session_secret] || SecureRandom.hex(16)
 install_dir = node[:redmine][:install_dir]
 install_base_dir = File.dirname(install_dir)
 database_name = 'redmine'
 tarball_cache = "#{Chef::Config[:file_cache_path]}/redmine-#{version}.tar.gz"
+webserver_type = node[:webserver_type]
+webserver_user = node[:webserver_user]
 
 package 'mysql-devel'
+package 'ImageMagick-devel'
+
+if version.to_f >= 1.4
+  gem_package 'bundler'
+else
+  gem_package 'rack' do
+    version '1.1.1'
+  end
+  gem_package 'rake' do
+    version '0.8.7'
+  end
+  gem_package 'i18n' do
+    version '0.4.2'
+  end
+  gem_package 'mysql' do
+    version '2.8.1'
+  end
+  gem_package 'rails' do
+    version '2.3.14'
+  end
+  gem_package 'coderay' do
+    version '1.0.4'
+  end
+  gem_package 'rw_fastercsv' do
+    version '1.5.7'
+  end
+end
 
 remote_file tarball_cache do
-  source "http://rubyforge.org/frs/download.php/76013/redmine-#{version}.tar.gz"
+  source "http://rubyforge.org/frs/download.php/76134/redmine-#{version}.tar.gz"
   not_if { File.exists? tarball_cache }
+end
+
+directory install_base_dir do
+  owner webserver_user
+  group webserver_user
+  action :create
 end
 
 bash 'install_redmine' do
@@ -24,7 +60,6 @@ bash 'install_redmine' do
   code <<-EOH
     tar xf #{Chef::Config[:file_cache_path]}/redmine-#{version}.tar.gz &&
     cd #{install_dir} &&
-    cp config/database.yml.example config/database.yml &&
     cat <<EOF | patch -p0 &&
 --- app/controllers/application_controller.rb.orig      2012-04-14 17:21:00.000000000 +0900
 +++ app/controllers/application_controller.rb   2012-04-18 10:09:22.596805018 +0900
@@ -39,20 +74,38 @@ bash 'install_redmine' do
        # auto-login feature starts a new session
        user = User.try_to_autologin(cookies[:autologin])
 EOF
-    chown -R apache:apache #{install_dir}
+    chown -R #{webserver_user}:#{webserver_user} #{install_dir}
   EOH
   not_if { File.exists? "#{install_dir}/config/database.yml" }
 end
 
-template "#{install_dir}/config/additional_environment.rb" do
-  source "additional_environment.rb.erb"
-  mode 0400
-  owner "apache"
-  group "apache"
-  variables(
-    :session_secret => session_secret
-  )
-  not_if { File.exists? "#{install_dir}/config/additional_environment.rb" }
+template "#{install_dir}/config/database.yml" do
+  source "database.yml.erb"
+  mode 0666
+  owner webserver_user
+  group webserver_user
+  not_if { File.exists? "#{install_dir}/config/database.yml" }
+end
+
+bash 'install_bundles' do
+  cwd install_dir
+  code <<-EOH
+    bundle install --without development test postgresql sqlite
+  EOH
+  only_if { version.to_f >= 1.4 }
+end
+
+if version.to_f <= 1.4
+  template "#{install_dir}/config/additional_environment.rb" do
+    source "additional_environment.rb.erb"
+    mode 0400
+    owner webserver_user
+    group webserver_user
+    variables(
+      :session_secret => session_secret
+    )
+    not_if { File.exists? "#{install_dir}/config/additional_environment.rb" }
+  end
 end
 
 link "/var/www/html/redmine" do
@@ -63,28 +116,11 @@ class Chef::Resource::Bash
   include Mysql
 end
 
-gem_package 'rack' do
-  version '1.1.1'
+if version.to_f >= 2.0
+  session_create_task = "generate_secret_token"
+else
+  session_create_task = "generate_session_store"
 end
-gem_package 'rake' do
-  version '0.8.7'
-end
-gem_package 'i18n' do
-  version '0.4.2'
-end
-gem_package 'mysql' do
-  version '2.8.1'
-end
-gem_package 'rails' do
-  version '2.3.14'
-end
-gem_package 'coderay' do
-  version '1.0.4'
-end
-gem_package 'rw_fastercsv' do
-  version '1.5.7'
-end
-
 bash 'setup_redmine_database' do
   code <<-EOH
     mysql -uroot <<EOF &&
@@ -93,12 +129,21 @@ DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 EOF
     cd #{install_dir} &&
-    rake generate_session_store &&
-    rake db:migrate RAILS_ENV=production
+    rake #{session_create_task} &&
+    RAILS_ENV=production rake db:migrate
   EOH
   not_if { has_database? database_name }
 end
 
-template '/etc/httpd/conf.d/redmine.conf' do
-  source 'redmine.conf.erb'
+if webserver_type == 'apache'
+  package 'mod_ssl'
+  template '/etc/httpd/conf.d/redmine.conf' do
+    source 'redmine.conf.erb'
+    mode 0666
+    owner webserver_user
+    group webserver_user
+    variables(
+      :ruby_path => ruby_path
+    )
+  end
 end
