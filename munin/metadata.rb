c6c02e93c8e0d99f63fbec6c33e1cf14cb1bf937
Hiroaki Nakamura <hnakamur@gmail.com>
2012-07-12 20:11:59 +0900

    add munin cookbook

---
+++ munin/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Hiroaki Nakamura"
+maintainer_email ""
+license          "MIT"
+description      "Installs/Configures munin"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends          "perl"
+depends          "rrdtool"
+depends          "apache"
---
+++ munin/recipes/default.rb
@@ -0,0 +1,92 @@
+#
+# Cookbook Name:: munin
+# Recipe:: default
+#
+# Copyright 2012, Hiroaki Nakamura
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+version = node[:munin][:version]
+
+group 'munin' do
+  gid node[:munin][:gid]
+end
+
+user 'munin' do
+  uid node[:munin][:uid]
+  gid 'munin'
+  comment 'Munin networked resource monitoring tool'
+end
+
+%w{ make gcc cronie }.each do |pkg|
+  package pkg
+end
+
+bash 'munin_install_perl_modules' do
+  code <<-EOH
+    cpanm Time::HiRes Storable Digest::MD5 HTML::Template Text::Balanced \
+      Params::Validate Net::SSLeay Getopt::Long \
+      File::Copy::Recursive CGI::Fast IO::Socket::INET6 Log::Log4perl \
+      Log::Dispatch Log::Dispatch::FileRotate MIME::Lite \
+      Mail::Sendmail URI FCGI
+      # These modules were not found.
+      # TimeDate IPC::Shareable Mail::Sender MailTools
+  EOH
+end
+
+remote_file "/usr/local/src/munin-#{version}.tar.gz" do
+  source "http://downloads.sourceforge.net/project/munin/stable/#{version}/munin-#{version}.tar.gz"
+  case version
+  when "2.0.2"
+    checksum "e8a5266a85cde8b89a97fb7463a56a7ac9c038035b952e36047b7d599bb9181b"
+  end
+end
+
+bash 'install_munin' do
+  cwd '/usr/local/src/'
+  code <<-EOH
+    tar xf munin-#{version}.tar.gz &&
+    cd munin-#{version} &&
+    make &&
+    make install &&
+    chmod 777 /opt/munin/log/munin /var/opt/munin/cgi-tmp
+  EOH
+  not_if { FileTest.exists?("/opt/munin/lib/munin-asyncd") }
+end
+
+bash 'create_munin-htpasswd' do
+  code <<-EOH
+    htpasswd -b -c /etc/opt/munin/munin-htpasswd "#{node[:munin][:web_interface_login]}" "#{node[:munin][:web_interface_password]}" 
+  EOH
+  not_if { FileTest.exists?("/etc/opt/munin/munin-htpasswd") }
+end
+
+cookbook_file "/etc/cron.d/munin" do
+  source "munin.cron"
+  owner 'root'
+  group 'root'
+  mode 0644
+  not_if { FileTest.exists?("/etc/cron.d/munin") }
+end
+
+service 'crond' do
+  action [:enable, :start]
+end
