da5759278e6d3491508f81594d3a1e18cfe42478
Hiroaki Nakamura <hnakamur@gmail.com>
2012-07-14 07:58:34 +0900

    Now we install perlbrew before running chef-solo. This change is needed because we must run 'source /root/.bashrc' after installing perlbrew.

--- munin/metadata.rb
+++ munin/metadata.rb
@@ -4,6 +4,5 @@ license          "MIT"
 description      "Installs/Configures munin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-depends          "perl"
 depends          "rrdtool"
 depends          "apache"
--- rrdtool/metadata.rb
+++ rrdtool/metadata.rb
@@ -5,4 +5,3 @@ description      "Installs/Configures rrdtool"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends          "glib"
-depends          "perl"
