146302b64e749e5ef2b996552c4b6a4acdcb584a
Hiroaki Nakamura <hnakamur@gmail.com>
2012-07-16 05:54:38 +0900

    Remove perl cookbook. Install shell script instead before running chef-solo, because we need to run "source /root/.bashrc" after installation.

--- perl/files/default/install_perlbrew.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PERLBREW_ROOT=/usr/local/perlbrew
-curl -kL http://install.perlbrew.pl | bash
-echo "source ${PERLBREW_ROOT}/etc/bashrc" >> /root/.bashrc &&
-. /root/.bashrc &&
-perlbrew init
