4a8f5db12a700ebf1440e305c4dbb681701edaa1
Hiroaki Nakamura <hnakamur@gmail.com>
2012-08-13 18:44:06 +0900

    Add node_http_proxy cookbook.

---
+++ node_http_proxy/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Hiroaki Nakamura"
+maintainer_email ""
+license          "MIT"
+description      "Installs/Configures node_http_proxy"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends          "nodejs"
