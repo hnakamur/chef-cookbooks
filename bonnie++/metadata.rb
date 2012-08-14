515e5971ff4a7d7aada918d2f04fc12e39a8c0d0
Hiroaki Nakamura <hnakamur@gmail.com>
2012-08-14 14:53:03 +0900

    Add bonnie++ cookbook.

---
+++ bonnie++/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Hiroaki Nakamura"
+maintainer_email ""
+license          "MIT"
+description      "Installs/Configures bonnie++"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ bonnie++/recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: bonnie++
+# Recipe:: default
+#
+# Copyright 2012, Hiroaki Nakamura
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+version = node[:"bonnie++"][:version]
+
+%w{ make gcc }.each do |pkg|
+  package pkg
+end
+
+remote_file "/usr/local/src/bonnie++-#{version}.tgz" do
+  source "http://www.coker.com.au/bonnie++/bonnie++-#{version}.tgz"
+  case version
+  when "1.03e"
+    checksum "cb3866116634bf65760b6806be4afa7e24a1cad6f145c876df8721f01ba2e2cb"
+  end
+end
+
+bash 'install_bonnie++' do
+  cwd '/usr/local/src/'
+  code <<-EOH
+    tar xf /usr/local/src/bonnie++-#{version}.tgz &&
+    ./configure &&
+    make &&
+    make install
+  EOH
+  not_if { FileTest.exists?("/usr/local/sbin/bonnie++") }
+end
