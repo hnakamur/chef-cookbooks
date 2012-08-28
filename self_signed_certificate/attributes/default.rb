afe68e07c8000deb4e00bb38ad68f7121008e396
Hiroaki Nakamura <hnakamur@gmail.com>
2012-08-28 12:02:49 +0900

    Add cookbook for creating self-signed certificate.

---
+++ self_signed_certificate/attributes/default.rb
@@ -0,0 +1,7 @@
+default.ssl_certificate = {
+  serial: 0,
+  days: 365,
+  subject: "/C=JP/ST=Kanagawa/L=Yokohama City/CN=test2.example.com",
+  crt_file: "/etc/pki/tls/certs/test2.example.com.crt",
+  key_file: "/etc/pki/tls/private/test2.example.com.key"
+}
