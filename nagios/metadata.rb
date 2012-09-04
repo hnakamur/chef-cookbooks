e891965d63b109670ab21bcb80ec2abb29b8be8a
Hiroaki Nakamura <hnakamur@gmail.com>
2012-09-04 17:22:16 +0900

    Removed left-over dependencies to nagios_user cookbook.

--- nagios/metadata.rb
+++ nagios/metadata.rb
@@ -5,4 +5,3 @@ description      "Installs/Configures nagios"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends          "php"
-depends          "nagios_user"
