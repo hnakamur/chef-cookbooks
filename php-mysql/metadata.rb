0f7bb77d6beedaaea3fe74612fb9674459a788a6
Hiroaki Nakamura <hnakamur@gmail.com>
2012-09-26 17:07:20 +0900

    Add php-mysql cookbook.

---
+++ php-mysql/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Hiroaki Nakamura"
+maintainer_email ""
+license          "MIT"
+description      "Installs/Configures php-mysql"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends          "php"
+depends          "mysql"
