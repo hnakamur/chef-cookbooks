9ac856449683012a1f8ae5139b1c831ae5dc9e32
Hiroaki Nakamura <hnakamur@gmail.com>
2012-12-21 07:31:35 +0900

    Rename munin_node cookbook to munin-node

--- munin-node/metadata.rb
+++ munin-node/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Hiroaki Nakamura"
 maintainer_email ""
 license          "MIT"
-description      "Installs/Configures munin_node"
+description      "Installs/Configures munin-node"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
