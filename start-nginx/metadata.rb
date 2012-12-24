74db059052828196b35f3b6ef6086d11acf2c12e
Hiroaki Nakamura <hnakamur@gmail.com>
2012-12-24 19:02:45 +0900

    Add start-nginx cookbook.

---
+++ start-nginx/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Hiroaki Nakamura"
+maintainer_email "hnakamur@gmail.com"
+license          "MIT"
+description      "Installs/Configures start-nginx"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends          "nginx"
